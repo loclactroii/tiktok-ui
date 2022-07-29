1dcdcbdbbd590defa38c122da224a754df9828f3
loclactroii <97525780+loclactroii@users.noreply.github.com>
2022-07-29 12:04:56 +0700

    1.babel-plugin-module-resolver

---
+++ src/components/Button/index.js
@@ -0,0 +1,7 @@
+import alert from '~/show'
+
+function Button() {
+    return <button onClick={alert}>Click me!</button>
+}
+
+export default Button
---
+++ src/show.js
@@ -0,0 +1,5 @@
+const alert = () => {
+    console.log('Successfully!')
+}
+
+export default alert
